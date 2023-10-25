755c5940b630e2fc89e93e3c8e44db730ae6729f
gloureirogomes <loureirobiel@gmail.com>
2023-10-25 00:42:09 -0300

    :sparkles: feat: implementing birthday cake candles challenge

--- birthday_cake_candles/birthday_cake_candles.go
+++ birthday_cake_candles/birthday_cake_candles.go
@@ -1,5 +1,20 @@
 package birthdaycakecandles
 
+import (
+	"sort"
+)
+
 func BirthdayCakeCandles(candles []int32) int32 {
-	
+	sort.Slice(candles, func(i, j int) bool {
+		return candles[i] < candles[j]
+	})
+
+	tallCandles := int32(0)
+	for i := 0; i < len(candles); i++ {
+		if candles[i] == candles[len(candles)-1] {
+			tallCandles++
+			continue
+		}
+	}
+	return tallCandles
 }
