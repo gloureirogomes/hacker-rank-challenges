b546bcf90a766b1b3e38a0e1cc1ec4a2bde209cc
gloureirogomes <loureirobiel@gmail.com>
2023-10-26 10:04:50 -0300

    :recycle: refactor: add all challenges runner functions at the main file

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	// birthdaycakecandles "hacker-rank/birthday_cake_candles"
+	// comparethetriplets "hacker-rank/compare_the_triplets"
+	// diagonaldifference "hacker-rank/diagonal_difference"
+	// simplearraysum "hacker-rank/simple_array_sum"
+	// solvemefirst "hacker-rank/solve_me_first"
+	// timeconversion "hacker-rank/time_conversion"
+	// verybigsum "hacker-rank/very_big_sum"
 )
 
 func main() {
 	fmt.Println("Hacker Rank Challenges!!!")
+	// Uncomment the line below and the import to run the desired package and function
+
+	// fmt.Println(solvemefirst.SumTwoNumbers())
+	// fmt.Println(simplearraysum.SumArray())
+	// fmt.Println(comparethetriplets.CompareTriplets())
+	// fmt.Println(verybigsum.VeryBigSum())
+	// fmt.Println(diagonaldifference.DiagonalDifference())
+	// fmt.Println(birthdaycakecandles.BirthdayCakeCandles())
+	// fmt.Println(timeconversion.TimeConversion())
+	// plusminus.PlusMinus()
+	// staircase.Staircase()
+	// minimaxsum.MiniMaxSum()
 }
